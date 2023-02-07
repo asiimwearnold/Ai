c0ea46a8457ac6f488154f5a37b26142a683b8f6
asiimawearnold <asiimwearnold25@gmail.com>
2023-02-08 00:03:20 +0300

    Fixed loading Eror

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
-
+//console.log(process.env.OPENAI_API_KEY)
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
+    
 })
 
 const openai = new OpenAIApi(configuration);
@@ -17,22 +18,22 @@ app.use(express.json());
 
 app.get('/', async (req, res) => {
     res.status(200).send({
-        message: 'hello from  Ai'
+        message: ''
     })
 });
 
 app.post('/', async (req, res) => {
     try {
         const prompt = req.body.prompt;
-        const respnse = await openai.createCompletion({
+        const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${ prompt }`,
-            temperature: 0.7,
+            temperature: 0,
             max_tokens: 3000,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,
-            stop: ["\"\"\""]
+           // stop: ["\"\"\""]
         });
 
         res.status(200).send({
